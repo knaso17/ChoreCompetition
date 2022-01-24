f7f920d03fd30bc6d11d93ffd3c414b0baa6ddf3
knaso17 <90427458+knaso17@users.noreply.github.com>
2022-01-24 15:52:33 -0600

    testing deploy

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const Home = () => (
   <div>
-    <h1>This is the Home Page</h1>
+    <h1>Home Page</h1>
     <p>
       Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras gravida,
       risus at dapibus aliquet, elit quam scelerisque tortor, nec accumsan eros
